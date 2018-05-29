46d041141bfea0acf292c52330d7924d9946accf
Michael Eickenberg <michael.eickenberg@gmail.com>
2018-05-29 10:51:14 -0700

    more stuff

--- newsgroups.py
+++ newsgroups.py
@@ -71,15 +71,37 @@ elif search_type == 'hyperopt':
     import hyperopt as hp
     from sklearn.model_selection import cross_val_score
     from sklearn.utils import clone
-    def f_opt(**params):
-        p = clone(pipeline)
+    from hyperopt import fmin, tpe, hp, STATUS_OK
 
+    param_names = list(param_grid.keys())
+    param_values_ = [param_grid[key] for key in param_names]
+
+    inputs = []
+    outputs = []
+    def f_opt(param_tuple, param_names=param_names):
+        params = dict(zip(param_names, param_tuple))
+        p = clone(pipeline)
+        p.set_values(**params)
         scores = cross_val_score(p, newsgroups_train.data,
                                             newsgroups_train.target,
                                             cv=cv, n_jobs=10)
-        return scores.mean()
-        
 
+        inputs.append(param_tuple)
+        output = scores.mean()
+        outputs.append(output)
+        return {'loss': output, 'status': STATUS_OK}
+    
+    # Have to set this here by hand. Would be great if it could be inferred
+    # from the specs in param_grid ...
+    space = (hp.loguniform('alpha', -5, 10),
+             hp.uniform('l1_ratio', 0, 1))
+
+    best = fmin(f_opt, space=space, algo=tpe.suggest, max_evals=100)    
+    # Do some hacky stuff to be able to use the save code below :D
+    hyperparam_searcher = lambda x: None
+    hyperparam_searcher.cv_results_ = dict(mean_test_score=np.array(outputs)
+                         params=[dict(zip(param_names, values))
+                             for values in inputs])
 
 np.save('data/score_newsgroups_%s_%s.npy' % (classifier_type, search_type),
         hyperparam_searcher.cv_results_['mean_test_score'])
