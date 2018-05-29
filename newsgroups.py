fbd4762cd8f3ac84d7663c373fe2aae848615e3b
Michael Eickenberg <michael.eickenberg@gmail.com>
2018-05-29 10:58:30 -0700

    hyperopt looks like its running

--- newsgroups.py
+++ newsgroups.py
@@ -77,7 +77,7 @@ elif search_type == 'skopt':
 elif search_type == 'hyperopt':
     import hyperopt as hp
     from sklearn.model_selection import cross_val_score
-    from sklearn.utils import clone
+    from sklearn.base import clone
     from hyperopt import fmin, tpe, hp, STATUS_OK
 
     param_names = list(param_grid.keys())
@@ -88,7 +88,7 @@ elif search_type == 'hyperopt':
     def f_opt(param_tuple, param_names=param_names):
         params = dict(zip(param_names, param_tuple))
         p = clone(pipeline)
-        p.set_values(**params)
+        p.set_params(**params)
         scores = cross_val_score(p, newsgroups_train.data,
                                             newsgroups_train.target,
                                             cv=cv, n_jobs=NJOBS)
@@ -105,7 +105,7 @@ elif search_type == 'hyperopt':
     best = fmin(f_opt, space=space, algo=tpe.suggest, max_evals=100)    
     # Do some hacky stuff to be able to use the save code below :D
     hyperparam_searcher = lambda x: None
-    hyperparam_searcher.cv_results_ = dict(mean_test_score=np.array(outputs)
+    hyperparam_searcher.cv_results_ = dict(mean_test_score=np.array(outputs),
                          params=[dict(zip(param_names, values))
                              for values in inputs])
 
