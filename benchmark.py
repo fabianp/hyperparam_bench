b84ac9dfd2a269db3aae6ba7d348eaca96b430ba
Michael Eickenberg <michael.eickenberg@gmail.com>
2018-05-30 12:04:00 -0700

    clean up benchmark.py - essentially remove commented stuff

--- benchmark.py
+++ benchmark.py
@@ -14,57 +14,18 @@ for seed in range(10):
     Xtrain, ytrain, cv, classifiers = (newsgroups_info[item]
                 for item in ('Xtrain', 'ytrain', 'cv', 'classifiers'))
 
-    ## Data
-    #from sklearn.datasets import fetch_20newsgroups
-    #categories = None
-    #newsgroups_train = fetch_20newsgroups(categories=categories,
-    #                    subset='train', remove=('headers', 'footers', 'quotes'))
-
-
-    ## Preproc
-    #from sklearn.feature_extraction.text import TfidfVectorizer
-    #vectorizer = TfidfVectorizer()
-
-
-    # Classifier
     classifier_type = sys.argv[1]
 
-    #from sklearn.naive_bayes import MultinomialNB
-    #from sklearn.linear_model import SGDClassifier
-    #
     if classifier_type == 'mnb':
-    #    clf = MultinomialNB()
-    #    param_grid = dict(classifier__alpha=np.logspace(-3, 3, 1000))
-        pipeline, param_grids = classifiers[1]
+       pipeline, param_grids = classifiers[1]
     elif classifier_type == 'sgd':
-    #    clf = SGDClassifier(loss='hinge', penalty='elasticnet')
-    #    param_grid = dict(classifier__alpha=np.logspace(-4, 4, 1000),
-    #                        classifier__l1_ratio=np.linspace(0, 1, 1000))
-    #    param_grid_skopt = dict(
-    #        classifier__alpha=(1e-4, 1e4, 'log-uniform'), classifier__l1_ratio=(0, 1, 'uniform'))
-        pipeline, param_grids = classifiers[0]
+       pipeline, param_grids = classifiers[0]
 
     param_grid = param_grids['hyperparams_random_search']
     param_grid_skopt = param_grids['hyperparams_skopt']
     param_grid_hyperopt = param_grids['hyperparams_hyperopt']
 
-    ## Pipeline
-    #from sklearn.pipeline import Pipeline
-    #pipeline = Pipeline((('vectorizer', vectorizer),
-    #                    ('classifier', clf)))
-    #
-
-    ## Cross validation splits
-    #from sklearn.model_selection import StratifiedShuffleSplit, ShuffleSplit
-    #cv = StratifiedShuffleSplit(random_state=0)
-    #
-
-    # Grid searches
-
     search_type = sys.argv[2]
-
-    # ^ we might want to make this a distribution?
-
 
 
     if search_type == 'random':
@@ -79,7 +40,7 @@ for seed in range(10):
     elif search_type == 'grid':
         # This simply depends on where in the enumeration
         # the best value is. How can we properly compare this?
-        pass
+        raise NotImplementedError("Currently no grid search")
     elif search_type == 'skopt':
         from skopt import BayesSearchCV
 
@@ -109,11 +70,6 @@ for seed in range(10):
             output = scores.mean()
             outputs.append(output)
             return {'loss': output, 'status': STATUS_OK}
-
-#        # Have to set this here by hand. Would be great if it could be inferred
-#        # from the specs in param_grid ...
-#        space = (hp.loguniform('alpha', -5, 10),
-#                 hp.uniform('l1_ratio', 0, 1))
 
         best = fmin(f_opt, space=param_values, algo=tpe.suggest, max_evals=100)
         # Do some hacky stuff to be able to use the save code below :D
